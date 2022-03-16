398a4dee4d31c7f4fbd9732a1643620d3a065974
Dana White <dana.white@stackhawk.com>
2022-03-16 16:28:42 -0600

    More things

--- dist/index.js
+++ dist/index.js
@@ -16072,7 +16072,7 @@ let childProcessId = -1;
 
 function killChildProcess() {
     core.debug(`Killing process ${childProcessId}`)
-    process.kill(Number(childProcessId), 'SIGINT');
+    process.kill(Number(childProcessId), 'SIGTERM');
 }
 
 module.exports.addSignalHandler = function addSignalHandler(){
@@ -16083,14 +16083,7 @@ module.exports.addSignalHandler = function addSignalHandler(){
 
     process.on('SIGTERM', () => {
         core.debug(`SIGTERM received for ${process.pid}`);
-        if (process.pid !== childProcessId){
-            killChildProcess();
-
-            setTimeout(() => {
-                console.log('Exiting.');
-                process.exit(1);
-            }, 1000);
-        }
+        killChildProcess();
     });
 
     process.on('SIGHUP', () => {
--- src/signal_handler.js
+++ src/signal_handler.js
@@ -4,7 +4,7 @@ let childProcessId = -1;
 
 function killChildProcess() {
     core.debug(`Killing process ${childProcessId}`)
-    process.kill(Number(childProcessId), 'SIGINT');
+    process.kill(Number(childProcessId), 'SIGTERM');
 }
 
 module.exports.addSignalHandler = function addSignalHandler(){
@@ -15,14 +15,7 @@ module.exports.addSignalHandler = function addSignalHandler(){
 
     process.on('SIGTERM', () => {
         core.debug(`SIGTERM received for ${process.pid}`);
-        if (process.pid !== childProcessId){
-            killChildProcess();
-
-            setTimeout(() => {
-                console.log('Exiting.');
-                process.exit(1);
-            }, 1000);
-        }
+        killChildProcess();
     });
 
     process.on('SIGHUP', () => {
