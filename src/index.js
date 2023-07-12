43bd8e4e6e8ba5444034fbf0a18fba3f6d8238fe
d-co-white <98342720+d-co-white@users.noreply.github.com>
2023-07-12 15:52:55 -0600

    Feature/scan 1170/output scan (#217)
    * Add scan id to output

--- src/index.js
+++ src/index.js
@@ -22,6 +22,8 @@ async function run() {
       exitCode = scanData.exitCode;
       core.debug(`Scanner exit code: ${scanData.exitCode} (${typeof scanData.exitCode})`);
       core.debug(`Link to scan results: ${scanData.resultsLink} (${typeof scanData.resultsLink})`);
+      core.debug(`This is the scan id: ${scanData.scanId} (${typeof scanData.scanId})`);
+      core.setOutput("scanId", scanData.scanId);
     }
 
   }
